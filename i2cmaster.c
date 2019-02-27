00d37e3afe8e169ff8ac137c7d6f75cb179168d0
PanSzczerba <26200369+PanSzczerba@users.noreply.github.com>
2019-02-27 17:57:22 +0100

    Added source and header

---
+++ i2cmaster.c
@@ -0,0 +1,67 @@
+#include "i2cmaster.h"
+
+#define PRESCALER_MAX 3
+
+void i2c_init(i2c_bit_rate bit_rate)
+{
+    i2c_set_bit_rate(bit_rate);
+}
+
+void i2c_set_bit_rate(i2c_bit_rate bit_rate)
+{
+    uint32_t bit_rate_tmp;
+    switch(bit_rate)
+    {
+    case I2C_100KHZ:
+        bit_rate_tmp = 100000;
+        break;
+    case I2C_400KHZ:
+        bit_rate_tmp = 400000;
+        break;
+    case I2C_1MHZ:
+        bit_rate_tmp = 1000000;
+        break;
+    case I2C_3_2MHZ:
+        bit_rate_tmp = 3200000;
+        break;
+    }
+
+    uint8_t prescaler = 0;
+    bit_rate_tmp = (F_CPU / bit_rate_tmp - 16) / 2;
+    while(bit_rate_tmp >= UINT8_MAX && prescaler <= PRESCALER_MAX)
+    {
+        bit_rate_tmp >>= 2;
+        prescaler++;
+    }
+    if(bit_rate_tmp > UINT8_MAX)
+        bit_rate_tmp = UINT8_MAX;
+
+    TWBR = (uint8_t) bit_rate_tmp;
+    TWSR = prescaler;
+
+}
+
+uint16_t i2c_send_vec(const uint8_t* data, uint16_t count)
+{
+    uint16_t bytes_sent = 0;
+
+    for(; bytes_sent < count; bytes_sent++)
+        if(i2c_send_byte(data[bytes_sent]) != TW_MT_DATA_ACK)
+            return bytes_sent;
+
+    return bytes_sent;
+}
+
+uint16_t i2c_receive_vec(uint8_t* data, uint16_t count, bool last_nack)
+{
+    uint8_t bytes_received = 0;
+
+    for(; bytes_received < count - 1; bytes_received++)
+        if(i2c_receive_byte(&data[bytes_received], true) != TW_MR_DATA_ACK)
+            return bytes_received;
+
+    if(i2c_receive_byte(&data[bytes_received], !last_nack) == TW_MR_DATA_NACK)
+        bytes_received++;
+
+    return bytes_received;
+}
---
+++ i2cmaster.h
@@ -0,0 +1,80 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+#include <avr/io.h>
+#include <util/twi.h>
+
+typedef enum
+{
+    I2C_READ = TW_READ,
+    I2C_WRITE = TW_WRITE
+} transfer_mode;
+
+typedef enum
+{
+    I2C_100KHZ,
+    I2C_400KHZ,
+    I2C_1MHZ,
+    I2C_3_2MHZ
+} i2c_bit_rate;
+
+inline void i2c_stop()
+{
+    TWCR = _BV(TWINT) | _BV(TWSTO) | _BV(TWEN);
+}
+
+inline uint8_t i2c_start()
+{
+    TWCR = _BV(TWINT) | _BV(TWSTA) | _BV(TWEN);
+
+    loop_until_bit_is_set(TWCR, TWINT);
+    return TW_STATUS;
+}
+
+inline uint8_t i2c_rep_start()
+{
+    TWCR = _BV(TWINT) | _BV(TWSTA) | _BV(TWEN);
+
+    loop_until_bit_is_set(TWCR, TWINT);
+    return TW_STATUS;
+}
+
+inline uint8_t i2c_send_sla(uint8_t slave_address, transfer_mode mode)
+{
+    TWDR = (slave_address << 1) | mode;
+
+    TWCR = _BV(TWINT) | _BV(TWEN);
+    loop_until_bit_is_set(TWCR, TWINT);
+    return TW_STATUS;
+}
+
+inline uint8_t i2c_send_byte(uint8_t data)
+{
+    TWDR = data;
+
+    TWCR = _BV(TWINT) | _BV(TWEN);
+    loop_until_bit_is_set(TWCR, TWINT);
+    return TW_STATUS;
+}
+
+inline uint8_t i2c_receive_byte(uint8_t* data, bool send_ack)
+{
+    TWCR = _BV(TWINT) | (send_ack ? _BV(TWEA) : 0) | _BV(TWEN);
+
+    loop_until_bit_is_set(TWCR, TWINT);
+    *data = TWDR;
+    return TW_STATUS;
+}
+
+/* Just sets bitrate */
+void i2c_init(i2c_bit_rate bit_rate);
+
+/* Sets bitrate of hardware I2C */
+void i2c_set_bit_rate(i2c_bit_rate bit_rate);
+
+/* Sends byte vector via I2C */
+uint16_t i2c_send_vec(const uint8_t* data, uint16_t count);
+
+/* Recives byte vector via I2C, if last vector byte should be
+ * the one terminating receive operation set last_nack to true */
+uint16_t i2c_receive_vec(uint8_t* data, uint16_t count, bool last_nack);
